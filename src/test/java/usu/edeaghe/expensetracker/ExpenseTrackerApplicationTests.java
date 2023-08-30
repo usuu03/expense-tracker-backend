26c07d817a906f77f3b50bbe3918bf2348c14360
Usu <usuedeaghe@gmail.com>
2023-08-30 20:56:10 +0100

    Initial commit

---
+++ src/test/java/usu/edeaghe/expensetracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package usu.edeaghe.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
